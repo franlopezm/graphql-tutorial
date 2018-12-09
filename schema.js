f54c2fe9ef366e61feb694ca5a3a2907c5d713b1
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2018-12-09 19:59:39 +0100

    Added id in schema and author to find one author by id

--- schema.js
+++ schema.js
@@ -3,13 +3,16 @@ import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
   type Author {
+    id: Int
     age: Int
     name: String
-    Books: [String]
+    books: [String]
+    #gender: String
   }
 
   type Query {
     authors: [Author]
+    author(id: Int): Author
   }
 `;
 
