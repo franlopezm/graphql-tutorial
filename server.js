785ea9e51cae2f47016c5af80fc2127cff0a81dd
Francisco Lopez Muñoz <frans.lopez.m@gmail.com>
2018-12-09 20:00:19 +0100

    Added resolvers to ApolloServer

--- server.js
+++ server.js
@@ -2,19 +2,15 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
 import typeDefs from './schema';
+import resolvers from './resolvers';
 
 const PORT = 4000;
 // path to run graphql server
 const PATH = '/gql';
 const app = express();
 
-/* const resolvers = {
-  Query: {
-    authors: () => authors
-  }
-} */
 
-const server = new ApolloServer({ typeDefs, /* resolvers */ });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: PATH });
 
 app.listen({ port: PORT }, () => {
